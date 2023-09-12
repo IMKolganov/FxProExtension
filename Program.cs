37af275ff4ff5bcc046f96e8f1b5189a13a38271
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2023-09-13 02:55:37 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FxPro;
+
+Console.WriteLine("Hello, World!");
+
+var s = "abcde";
+
+
+//abcde
+var result = s.Replace2("bc", "y");
+
+Console.WriteLine(result);
+
+
+//string input = "aabb";
+//string oldVal = "aab";
+//string newVal = "xyz";
+//res = input.Replace2(oldVal, newVal);
+
+
+Console.WriteLine(result);
+
+Console.WriteLine("FxPro is done!!!");
+
+Console.ReadLine();
---
+++ StringReplaseExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FxPro
+{
+    public static class StringReplaseExtension
+    {
+        public static string Replace2(this string current, string oldValue, string newValue)
+        {
+            StringBuilder str = new StringBuilder();
+
+
+
+            int howMuch = -1;
+            bool needSceep = false;
+            foreach (char simbol in current)
+            {
+                foreach (char findSimbol in oldValue)
+                {
+                    if (needSceep)
+                    {
+                        needSceep = false;
+                        continue;
+                    }
+
+                    if (simbol == findSimbol)
+                    {
+                        if (howMuch == -1)
+                        {
+                            needSceep = oldValue.Length > 1;
+                            howMuch = 1;
+                            break;
+                        }
+                        else
+                        {
+                            needSceep = true;
+                            howMuch++;
+                        }
+
+
+                        if (howMuch == oldValue.Length)
+                        {
+                            str.Append(newValue);
+                            howMuch = -1;
+                            needSceep = false;
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                        needSceep = false;
+                        howMuch = -1;
+                        str.Append(simbol);
+                        break;
+                    }
+                }
+            }
+            return str.ToString();
+        }
+    }
+}
